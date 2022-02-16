57695b7fcf5e0394d89fda793bee807daee9eb21
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-16 17:58:50 +0700

    Add files via upload

---
+++ inswssh.sh
@@ -0,0 +1,45 @@
+##upgrade
+clear
+echo Installing Websocket-SSH Python
+sleep 1
+echo Check Access Rights...
+sleep 0.5
+apt-get update
+apt-get upgrade -y
+cd
+cd /etc/systemd/system/
+wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/myridwan/vps/ipuk/ws-openssh.service
+cd
+cd /etc/systemd/system/
+wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/myridwan/vps/ipuk/ws-dropbear.service
+cd
+cd /etc/systemd/system/
+wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/myridwan/vps/ipuk/ws-tls.service
+cd
+cd /etc/systemd/system/
+wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/myridwan/vps/ipuk/ws-tls.py
+cd
+cd /etc/systemd/system/
+wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/myridwan/vps/ipuk/ws-dropbear.py
+cd
+cd /usr/local/bin/
+#Install Script Websocket-SSH Python
+wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/myridwan/vps/ipuk/ws-openssh.py
+chmod +x /usr/local/bin/ws-openssh
+##Screen
+apt-get install screen -y
+##python install
+apt-get install python -y
+
+cd
+systemctl daemon-reload
+#Enable & Start & Restart ws-openssh service
+systemctl enable ws-openssh.service
+systemctl enable ws-dropbear.service
+systemctl enable ws-tls.service
+systemctl start ws-openssh.service
+systemctl start ws-dropbear.service
+systemctl start ws-tls.service
+systemctl restart ws-openssh.service
+systemctl restart ws-dropbear.service
+systemctl restart ws-tls.service
